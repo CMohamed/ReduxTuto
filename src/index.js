ae3ae1b4b96701cc552d643c5a08a41661b30bd1
Mohamed CHADAD <chadad.mohamed@gmail.com>
2019-10-16 18:03:02 +0100

    [ReduxDocs] Basic tuto, store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import { createStore, compose } from 'redux'
+import todoApp from './reducers/reducers'
+import {ADD_TODO} from "./actions/actions";
+
+const store = createStore(
+    todoApp,
+    compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+);
+
+console.log(store.getState());
+
+store.dispatch({
+    type: ADD_TODO,
+    text: 'hhhh'
+});
+console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
